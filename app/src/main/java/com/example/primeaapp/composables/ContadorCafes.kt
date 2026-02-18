58d01d36cf868aae01a4b4106e3ec83dc0c9e043
Hector Garcia <hrugarcia@gmail.com>
2026-02-18 14:36:08 -0600

    modificacion a contador de cafes

--- app/src/main/java/com/example/primeaapp/composables/ContadorCafes.kt
+++ app/src/main/java/com/example/primeaapp/composables/ContadorCafes.kt
@@ -1,7 +1,10 @@
 package com.example.primeaapp.composables
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,14 +14,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.primeaapp.R
 
 
 @Composable
 fun ContadorCafes(modifier: Modifier){
     var contador by remember {mutableStateOf(0)}
 
-    Column(modifier = modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
+    Column(modifier = modifier.fillMaxWidth().statusBarsPadding(), horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.cafe),
+            contentDescription = "cafe",
+            modifier = Modifier.size(300.dp)
+        )
         Text("Te has tomado ${contador} cafes hoy")
         Button({contador++}) {
             Text("Tomar cafe")
